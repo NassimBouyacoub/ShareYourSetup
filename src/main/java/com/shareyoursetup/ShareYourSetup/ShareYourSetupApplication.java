f4029f570bbf145788a80a4b06708ed17858552f
nassim <nassim.bouyacoub.com>
2023-02-28 15:53:13 +0100

    start

---
+++ src/main/java/com/shareyoursetup/ShareYourSetup/ShareYourSetupApplication.java
@@ -0,0 +1,13 @@
+package com.shareyoursetup.ShareYourSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareYourSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareYourSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shareyoursetup/ShareYourSetup/ShareYourSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareyoursetup.ShareYourSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareYourSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
